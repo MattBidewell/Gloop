262ae1d6fa1a42f936146dcd3a61ebd913b9cb5a
MattBidewell <matthewbidewell@gmail.com>
2022-07-01 22:27:01 +0100

    Chore: add test runner script

---
+++ test_runner.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+GOEXEC=$(which go)
+
+cd "$(dirname "$0")"
+
+for dir in ./*;
+  do (
+    if [ -d "$dir" ];then
+      echo "\n*********"
+      echo "checking for tests in $dir"
+
+      # urgh dont judge. But slowing it down to make it viewable.
+      sleep 0.5
+      # please...dont judge.
+
+      for file in $dir/*;
+        do (
+          if [[ "$file" == *"test"* ]];then
+            printf '%s\n' "$file"
+            exec $GOEXEC test $dir
+            break
+          fi
+        )
+      echo "*********"
+      done
+    fi
+  )
+done
+
+echo "\n*********"
+echo "finished"
+echo "*********"
+
+        # cd $dir
+        # echo "*********"
+        # echo "checking for tests in ${PWD##*/}"
+        # echo "*********"
+        # exec $GOEXEC test ./${PWD##*/}

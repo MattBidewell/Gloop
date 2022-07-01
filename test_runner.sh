d6f192df78669e772fc8497816f5d7b1c2d7ca96
MattBidewell <matthewbidewell@gmail.com>
2022-07-01 22:48:40 +0100

    fixup! Deployment: add test script to GHA

--- test_runner.sh
+++ test_runner.sh
@@ -19,7 +19,7 @@ for dir in ./*;
       for file in $dir/*;
         do (
           echo $file
-          if [[ "$file" == *"test"* ]];then
+          if [ "$file" == *"test"* ];then
             printf '%s\n' "$file"
             exec $GOEXEC test $dir
             break
